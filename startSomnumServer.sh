5dc9b70ec693a47a077b1ddb76beb150a2f45156
Steven <szeyungng@gmail.com>
2024-03-07 11:52:39 -0700

    Update bash file

--- startSomnumServer.sh
+++ startSomnumServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/szeyu/somnumEEG
-xdg-open https://somnum-f9028.web.app/
-node backend/server.js
+/usr/bin/xdg-open https://somnum-f9028.web.app/
+/home/szeyu/.nvm/versions/node/v17.9.1/bin/node backend/server.js
 
